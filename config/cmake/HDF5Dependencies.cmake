82d20a7618a0ea434363675b133cdd54829bcebc
StupidBhh <stupidbhh@gmail.com>
2026-09-03 01:40:40 +0800

    cmake: Classify compression dependencies

--- config/cmake/HDF5Dependencies.cmake
+++ config/cmake/HDF5Dependencies.cmake
@@ -10,28 +10,31 @@
 
 include_guard (GLOBAL)
 
-function (hdf5_configure_dependencies private_libraries public_libraries system_libraries)
-  set_property (TARGET hdf5_dependencies PROPERTY HDF5_PRIVATE_LINK_LIBRARIES
-      "${private_libraries}"
+function (hdf5_configure_dependencies system_libraries compression_libraries public_libraries)
+  set_property (TARGET hdf5_dependencies PROPERTY HDF5_SYSTEM_LINK_LIBRARIES
+      "${system_libraries}"
+  )
+  set_property (TARGET hdf5_dependencies PROPERTY HDF5_COMPRESSION_LINK_LIBRARIES
+      "${compression_libraries}"
   )
   set_property (TARGET hdf5_dependencies PROPERTY HDF5_PUBLIC_LINK_LIBRARIES
       "${public_libraries}"
   )
-  set_property (TARGET hdf5_dependencies PROPERTY HDF5_SYSTEM_LINK_LIBRARIES
-      "${system_libraries}"
-  )
 endfunction ()
 
 function (hdf5_target_link_dependencies target)
-  get_property (private_libraries TARGET hdf5_dependencies
-      PROPERTY HDF5_PRIVATE_LINK_LIBRARIES
+  get_property (system_libraries TARGET hdf5_dependencies
+      PROPERTY HDF5_SYSTEM_LINK_LIBRARIES
+  )
+  get_property (compression_libraries TARGET hdf5_dependencies
+      PROPERTY HDF5_COMPRESSION_LINK_LIBRARIES
   )
   get_property (public_libraries TARGET hdf5_dependencies
       PROPERTY HDF5_PUBLIC_LINK_LIBRARIES
   )
 
-  if (private_libraries)
-    target_link_libraries (${target} PRIVATE ${private_libraries})
+  if (system_libraries OR compression_libraries)
+    target_link_libraries (${target} PRIVATE ${system_libraries} ${compression_libraries})
   endif ()
   if (public_libraries)
     target_link_libraries (${target} PUBLIC ${public_libraries})
@@ -48,5 +51,18 @@ function (hdf5_target_link_system_dependencies target visibility)
   )
   if (system_libraries)
     target_link_libraries (${target} ${visibility} ${system_libraries})
+  endif ()
+endfunction ()
+
+function (hdf5_target_link_compression_dependencies target visibility)
+  if (NOT visibility MATCHES "^(PRIVATE|PUBLIC|INTERFACE)$")
+    message (FATAL_ERROR "Invalid dependency visibility for ${target}: ${visibility}")
+  endif ()
+
+  get_property (compression_libraries TARGET hdf5_dependencies
+      PROPERTY HDF5_COMPRESSION_LINK_LIBRARIES
+  )
+  if (compression_libraries)
+    target_link_libraries (${target} ${visibility} ${compression_libraries})
   endif ()
 endfunction ()
